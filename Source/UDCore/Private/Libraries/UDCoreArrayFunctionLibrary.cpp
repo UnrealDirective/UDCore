85f87a8a9337fb2990147f5aab3a714082edb542
Tezenari <djd1255@gmail.com>
2024-08-13 13:35:05 -0700

    Created UDCoreArrayFunctionLibrary
    - Added LastIndex and PreviousIndex functions

---
+++ Source/UDCore/Private/Libraries/UDCoreArrayFunctionLibrary.cpp
@@ -0,0 +1,90 @@
+﻿// © 2024 Unreal Directive. All rights reserved.
+
+
+#include "Libraries/UDCoreArrayFunctionLibrary.h"
+
+int32 UUDCoreArrayFunctionLibrary::Array_NextIndex(const TArray<int32>& TargetArray, const int32 Index, const bool bLoop)
+{
+	// We should never hit this function, as the custom thunk layer should be calling the native handler directly.
+	check(0);
+	return 0;
+}
+
+int32 UUDCoreArrayFunctionLibrary::Array_PreviousIndex(
+	const TArray<int32>& TargetArray,
+	const int32 Index,
+	const bool bLoop)
+{
+	// We should never hit this function, as the custom thunk layer should be calling the native handler directly.
+	check(0);
+	return 0;
+}
+
+int32 UUDCoreArrayFunctionLibrary::GenericArray_NextIndex(
+	const void* TargetArray,
+	const FArrayProperty* ArrayProperty,
+	const int32 Index,
+	const bool bLoop)
+{
+	if (!TargetArray)
+	{
+		return INDEX_NONE;
+	}
+
+	const FScriptArrayHelper ArrayHelper(ArrayProperty, TargetArray);
+	const int32 NextIndex = Index + 1;
+
+	if(ArrayHelper.Num() == 0)
+	{
+		return INDEX_NONE;
+	}
+
+	if(NextIndex < 0)
+	{
+		return 0;
+	}
+
+	if (NextIndex <= ArrayHelper.Num() - 1)
+	{
+		return NextIndex;
+	}
+
+	if (bLoop)
+	{
+		return 0;
+	}
+	
+	return Index;
+}
+
+int32 UUDCoreArrayFunctionLibrary::GenericArray_PreviousIndex(
+	const void* TargetArray,
+	const FArrayProperty* ArrayProperty,
+	const int32 Index,
+	const bool bLoop)
+{
+	if (!TargetArray)
+	{
+		return INDEX_NONE;
+	}
+
+	const FScriptArrayHelper ArrayHelper(ArrayProperty, TargetArray);
+	const int32 PreviousIndex = Index - 1;
+
+	if(ArrayHelper.Num() == 0)
+	{
+		return INDEX_NONE;
+	}
+	
+	if (PreviousIndex >= 0)
+	{
+		return PreviousIndex;
+	}
+	
+	if (bLoop)
+	{
+		return ArrayHelper.Num() - 1;
+	}
+	
+	return Index;
+}
---
+++ Source/UDCore/Public/Libraries/UDCoreArrayFunctionLibrary.h
@@ -0,0 +1,87 @@
+﻿// © 2024 Unreal Directive. All rights reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintFunctionLibrary.h"
+#include "UDCoreArrayFunctionLibrary.generated.h"
+
+/**
+ * UUDCoreArrayFunctionLibrary
+ * A collection of array functions.
+ */
+UCLASS()
+class UDCORE_API UUDCoreArrayFunctionLibrary : public UBlueprintFunctionLibrary
+{
+	GENERATED_BODY()
+
+	/**
+	 * Returns the next index in the array.
+	 * If bLoop is enabled, the index will loop back to the beginning of the array.
+	 * If bLoop is disabled, the last index will be returned if the end of the array is reached.
+	 * @param TargetArray - The array to get the next index for.
+	 * @param Index - The current index.
+	 * @param bLoop - If true, the index will loop back to the beginning of the array.
+	 * @returns The next index in the array. If at the end and bLoop is false, the last index is returned.
+	 */
+	UFUNCTION(BlueprintPure, CustomThunk, meta=(DisplayName = "Next Index", CompactNodeTitle = "NEXT INDEX", ArrayParm = "TargetArray", BlueprintThreadSafe), Category="UDCore|Array")
+	static int32 Array_NextIndex(const TArray<int32>& TargetArray, const int32 Index, const bool bLoop);
+
+	/**
+	 * Returns the previous index in the array.
+	 * If bLoop is enabled, the index will loop back to the end of the array.
+	 * If bLoop is disabled, the first index will be returned if the beginning of the array is reached.
+	 * @param TargetArray - The array to get the previous index for.
+	 * @param Index - The current index.
+	 * @param bLoop - If true, the index will loop back to the end of the array.
+	 * @returns The previous index in the array. If at the beginning and bLoop is false, the first index is returned.
+	 */
+	UFUNCTION(BlueprintPure, CustomThunk, meta=(DisplayName = "Previous Index", CompactNodeTitle = "PREV INDEX", ArrayParm = "TargetArray", BlueprintThreadSafe), Category="UDCore|Array")
+	static int32 Array_PreviousIndex(const TArray<int32>& TargetArray, const int32 Index, const bool bLoop);
+
+	// Native functions that will be called by the below custom thunk layers, which read off the property address and call the appropriate native handler.
+	// Based off UKismetArrayLibrary implementation
+
+	static int32 GenericArray_NextIndex(const void* TargetArray, const FArrayProperty* ArrayProperty, int32 Index, bool bLoop);
+	static int32 GenericArray_PreviousIndex(const void* TargetArray, const FArrayProperty* ArrayProperty, int32 Index, bool bLoop);
+
+	DECLARE_FUNCTION(execArray_NextIndex)
+	{
+		Stack.MostRecentProperty = nullptr;
+		Stack.StepCompiledIn<FArrayProperty>(nullptr);
+		const void* ArrayAddr = Stack.MostRecentPropertyAddress;
+		const FArrayProperty* ArrayProperty = CastField<FArrayProperty>(Stack.MostRecentProperty);
+		if (!ArrayProperty)
+		{
+			Stack.bArrayContextFailed = true;
+			return;
+		}
+		P_GET_PROPERTY(FIntProperty, Index);
+		P_GET_UBOOL(bLoop);
+		P_FINISH;
+		P_NATIVE_BEGIN;
+
+		*static_cast<int32*>(RESULT_PARAM) = GenericArray_NextIndex(ArrayAddr, ArrayProperty, Index, bLoop);
+		P_NATIVE_END;
+	}
+
+	DECLARE_FUNCTION(execArray_PreviousIndex)
+	{
+		Stack.MostRecentProperty = nullptr;
+		Stack.StepCompiledIn<FArrayProperty>(nullptr);
+		const void* ArrayAddr = Stack.MostRecentPropertyAddress;
+		const FArrayProperty* ArrayProperty = CastField<FArrayProperty>(Stack.MostRecentProperty);
+		if (!ArrayProperty)
+		{
+			Stack.bArrayContextFailed = true;
+			return;
+		}
+		P_GET_PROPERTY(FIntProperty, Index);
+		P_GET_UBOOL(bLoop);
+		P_FINISH;
+		P_NATIVE_BEGIN;
+
+		*static_cast<int32*>(RESULT_PARAM) = GenericArray_PreviousIndex(ArrayAddr, ArrayProperty, Index, bLoop);
+		P_NATIVE_END;
+	}
+};
